ae6d62c08e4d5e05f23a13b274cfa6ee7ecdd8d3
codename11 <veljkos82@gmail.com>
2018-10-18 16:55:28 +0200

    Updates

--- precache-manifest.d955ede28c07644bce37c7532937a59c.js
+++ precache-manifest.d955ede28c07644bce37c7532937a59c.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/DrumMachine/static/js/runtime~main.ab99b251.js"
   },
   {
-    "revision": "1e5665cd2f31c5b0ca19",
-    "url": "/DrumMachine/static/js/main.1e5665cd.chunk.js"
+    "revision": "de2ab2a7f86b2c73a09e",
+    "url": "/DrumMachine/static/js/main.de2ab2a7.chunk.js"
   },
   {
     "revision": "7f4def8d1a0bcf8fc0dc",
     "url": "/DrumMachine/static/js/1.7f4def8d.chunk.js"
   },
   {
-    "revision": "1e5665cd2f31c5b0ca19",
+    "revision": "de2ab2a7f86b2c73a09e",
     "url": "/DrumMachine/static/css/main.7853f3c1.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/DrumMachine/static/css/1.f00207b8.chunk.css"
   },
   {
-    "revision": "01dea4a9f2003c70d7c6a54c55a24f71",
+    "revision": "7d7fbdb12fe9b4ef1de3fc9bb519f996",
     "url": "/DrumMachine/index.html"
   }
 ];
